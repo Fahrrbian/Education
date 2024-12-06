2d8aeb76d384333a2e9024f3a6df39b32eb1220f
Fahrrbian <fabianholtapel0@gmail.com>
2024-12-06 12:10:19 +0100

    initial

--- eclipse-workspace/ProjUebung4.5/src/main/java/de/uebung4_5/phonebook/PhoneBookEntry.java
+++ eclipse-workspace/ProjUebung4.5/src/main/java/de/uebung4_5/phonebook/PhoneBookEntry.java
@@ -1,5 +1,31 @@
 package de.uebung4_5.phonebook;
 
+import jakarta.persistence.*; 
+
+@Entity
 public class PhoneBookEntry {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id; 
+	
+	private String name; 
+	private String phoneNumber; 
+	private boolean isBusiness; 
+	
+	public PhoneBookEntry(String name, String phoneNumber, boolean isBusiness) {
+		super(); 
+		this.name = name; 
+		this.phoneNumber = phoneNumber; 
+		this.isBusiness = isBusiness; 
+	}
+	public PhoneBookEntry() {
+		super(); 
+	}
+	public String getName() {
+		return name; 
+	}
+	public void setName(String name) {
+		this.name = name; 
+	}
 }
